c42c6633a76606ccd3a9c46fbc1a57280887c5bb
Alexey Pavlov <laraziel@yandex.ru>
2021-10-29 17:04:51 +0300

    Added all VCs to StoryBoard and created classes for them

---
+++ YouTunes/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  YouTunes
+//
+//  Created by user on 29.10.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
