27f0490d8661deba40102bf4cbc750a3017e4a37
Alexey Pavlov <laraziel@yandex.ru>
2021-10-31 15:33:13 +0300

    Refactored NetworkService class to avoid code duplication.

--- YouTunes/Services/NetworkService.swift
+++ YouTunes/Services/NetworkService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkService {
     static let shared = NetworkService()
@@ -19,56 +20,57 @@ class NetworkService {
     
     // MARK: - Fetching artist's albums from iTunes API
     
-    func fetchAlbumsForTerm(_ searchTerm: String, completionHadler: @escaping (AlbumThumbnailInfo) -> Void) {
+    func fetchAlbumsForTerm(_ searchTerm: String, completionHandler: @escaping (AlbumThumbnailInfo) -> Void) {
         let formattedSearchString = searchTerm
             .trimmingCharacters(in: .whitespaces)
             .replacingOccurrences(of: " ", with: "+")
         let searchURLString = "https://itunes.apple.com/search?term=" + formattedSearchString + "&media=music&entity=album"
         
-        guard let url = URL(string: searchURLString) else { return }
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            
-            if let error = error{
-                print(error.localizedDescription)
-            } else if let data = data,
-                      let response = response as? HTTPURLResponse,
-                      response.statusCode == 200 {
-                do {
-                    if let albumThimbnailInfo = try self?.decoder.decode(AlbumThumbnailInfo.self, from: data) {
-                        completionHadler(albumThimbnailInfo)
-                    }
-                } catch {
-                    print("Can't decode JSON")
-                }
+        networkRequestFor(string: searchURLString) { details, _ in
+            guard let details = details else {
+                print("Data is nil")
+                return
             }
-        }.resume()
+            completionHandler(details)
+        }
     }
     
     // MARK: - Fetching album delails from iTunes API
     
-    func fetchAlbumDetailsForAlbumID(_ albumID: Int, completionHadler: @escaping (AlbumDetails) -> Void) {
+    func fetchAlbumDetailsForAlbumID(_ albumID: Int, completionHandler: @escaping (AlbumDetails) -> Void) {
         let searchURLString = "https://itunes.apple.com/lookup?id=\(albumID)&entity=song"
-        guard let url = URL(string: searchURLString) else { return }
+        
+        networkRequestFor(string: searchURLString) { _, details in
+            guard let details = details else {
+                print("Data is nil")
+                return
+            }
+            completionHandler(details)
+        }
+    }
+    
+    func networkRequestFor(string: String, completionHandler: @escaping (AlbumThumbnailInfo?, AlbumDetails?) -> Void){
+        
+        guard let url = URL(string: string) else { return }
+        
+        if !UIApplication.shared.canOpenURL(url) {
+            print("Request URL is unreachable. Please check your internet connection.")
+            return
+        }
         
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             
-            if let error = error{
+            if let error = error {
                 print(error.localizedDescription)
             } else if let data = data,
                       let response = response as? HTTPURLResponse,
-                      response.statusCode == 200 {                
-                do {
-                    if let albumDetails = try self?.decoder.decode(AlbumDetails.self, from: data) {
-                        completionHadler(albumDetails)
-                    }
-                } catch {
-                    print("Can't decode AlbumDetails JSON")
-                }
+                      response.statusCode == 200 {
+                
+                let thumbnailsInfo = try? self?.decoder.decode(AlbumThumbnailInfo.self, from: data)
+                let albumDetails = try? self?.decoder.decode(AlbumDetails.self, from: data)
+                
+                completionHandler(thumbnailsInfo, albumDetails)
             }
         }.resume()
-        
-        //URL for album details
-        //https://itunes.apple.com/lookup?id=211192863&entity=song
-        
     }
 }
